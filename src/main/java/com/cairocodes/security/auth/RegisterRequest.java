bcd2827613cb98fd1d930e53d86269d52b15a947
Cairo Felipe <cairofelipedev@gmail.com>
2024-04-21 22:39:35 -0300

    New codes and sqlserver

---
+++ src/main/java/com/cairocodes/security/auth/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.cairocodes.security.auth;
+
+import com.cairocodes.security.user.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+  private String firstname;
+  private String lastname;
+  private String email;
+  private String password;
+  private Role role;
+}
---
+++ src/main/java/com/cairocodes/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.cairocodes.security.token;
+
+public enum TokenType {
+  BEARER
+}
